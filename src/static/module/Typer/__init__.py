9df9162efc7e6f53bd56125e03a34d004c6d8928
Benoit Forgette <benoit.forgette@digital.security>
2020-05-06 18:53:42 +0200

    [DEVELOP][TYPER] add module typer to know the type of each object of the ast

---
+++ src/static/module/Typer/__init__.py
@@ -0,0 +1,14 @@
+
+from static.module.register import ModuleStaticCmd
+
+@ModuleStaticCmd("types", "grab type of elements", bool)
+class Tree:
+    """
+    Class Exemple of cresation static module
+    """
+    def __init__(self, package, tmp_dir, args):
+        if args.types:
+            from . import typer
+        #print("test")
+        return None
+
---
+++ src/static/module/Typer/typer.py
@@ -0,0 +1,62 @@
+from static.ast import Node
+
+import javalang
+
+class EltType:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+@Node("MethodDeclaration", "in")
+class MethodDeclarationIn:
+    @classmethod
+    def call(cls, r, self):
+        #parameters = []
+        #for parameter in self.elt.parameters:
+        #    parameters.append((parameter.name, parameter.type.name))
+            #print("%s : %s" % (parameter.name,
+            #    parameter.type.name), end='\t')
+        r["TyperMethod"].append(self.elt.parameters)
+        return r
+
+@Node("VariableDeclarator", "in")
+class VariableDeclaratorIn:
+    @classmethod
+    def call(cls, r, self):
+        # No Initilizer means unknow type
+        if not self.elt.initializer:
+            return r
+        # Complex type TODO
+        if type(self.elt.initializer) is javalang.tree.This or \
+                type(self.elt.initializer) is javalang.tree.MemberReference or \
+                type(self.elt.initializer) is javalang.tree.MethodInvocation or \
+                type(self.elt.initializer) is javalang.tree.BinaryOperation or \
+                type(self.elt.initializer) is javalang.tree.Literal or \
+                type(self.elt.initializer) is javalang.tree.SuperMethodInvocation:
+            return r
+        elt = EltType(self.elt.name, self.elt.initializer.type)
+        if len(r["TyperMethod"]) == 0:
+            r["TyperMethod"].append([elt])
+        else:
+            r["TyperMethod"][-1].append(elt)
+        return r
+
+@Node("MethodDeclaration", "out")
+class MethodDeclarationIn:
+    @classmethod
+    def call(cls, r, self):
+        r["TyperMethod"].pop()
+        return r
+
+@Node("File", "in")
+class File:
+    @classmethod
+    def call(cls, r, path):
+        r["TyperMethod"] = []
+        return r
+#
+#@Node("Init", "in")
+#class Init:
+#    @classmethod
+#    def call(cls, r):
+#        return r
