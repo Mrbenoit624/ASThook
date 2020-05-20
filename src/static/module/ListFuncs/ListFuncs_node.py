a5c0dfe854ad48a8431f5c9beb757801c70adf96
Benoit Forgette <benoit.forgette@digital.security>
2020-05-20 19:53:50 +0200

    [DEVELOP][LISTFUNCS] Add packages of the module

--- src/static/module/ListFuncs/ListFuncs_node.py
+++ src/static/module/ListFuncs/ListFuncs_node.py
@@ -7,7 +7,10 @@ class MethodDeclaration:
     @classmethod
     def call(cls, r, self):
         if r["list_funcs_class"] and re.search(Func.get_name(), self.elt.name):
-            Output.add_tree_mod("list_funcs", "func", "%s.%s" % (r["list_funcs_class"], self.elt.name))
+            fc = "%s.%s" % (r["list_funcs_class"], self.elt.name)
+            Output.add_tree_mod("list_funcs", "func", [
+                "%s%s" % (fc, " " * (80 - len(fc))),
+                r["package"]])
         return r
 
 @Node("ConstructorDeclaration", "in")
@@ -15,7 +18,10 @@ class ClassDeclarationIn:
     @classmethod
     def call(cls, r, self):
         if r["list_funcs_class"] and re.search(Func.get_name(), self.elt.name):
-            Output.add_tree_mod("list_funcs", "func", "%s.%s" % (r["list_funcs_class"], self.elt.name))
+            fc = "%s.%s" % (r["list_funcs_class"], self.elt.name)
+            Output.add_tree_mod("list_funcs", "func", [
+                "%s%s" % (fc, " " * (80 - len(fc))),
+                r["package"]])
         return r
 
 @Node("ClassDeclaration", "in")
