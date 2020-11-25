115fbb9a1f8a7b8084062c9d5a043bb981809bb3
adesnos <adesnos@google.com>
2020-11-25 10:43:50 +0100

    Expand the API keys detector

--- asthook/static/module/ApiKeyDetector/__init__.py
+++ asthook/static/module/ApiKeyDetector/__init__.py
@@ -1,45 +1,66 @@
 
 from asthook.static.module.register import ModuleStaticCmd, load_module
 from asthook.utils import Output
-#from Logger import debug
 from asthook.log import debug
 from pathlib import Path
+
+import re
 import xml.etree.ElementTree as ET
 
 @ModuleStaticCmd("api_keys", "find api keys", bool)
 class Tree:
     """
-    Find API keys
+    Find API keys in source code + a list of files
     """
     def __init__(self, package, tmp_dir, args):
-        #for e in logging.root.manager.loggerDict:
-        #    logging.getLogger(e).setLevel(logging.ERROR)
         from api_key_detector import detector
+
+        whitelist_extension = ['.js', '.xml', '.dex', '.properties', '.txt', '.java', '.smali']
+
         load_module("ApiKeyDetector", "keys")
         load_module("name_file", "name_file_node")
-        #hdlrs = logging.root.handlers
-        #for logger in range(len(logging.root.handlers)):
-        #    if type(hdlrs[logger]) is logging.StreamHandler:
-        #        hdlrs.pop(logger)
-        #        break
-        path_res = Path(tmp_dir + "/decompiled_app/apktools/res")
-        string_file = Path(str(path_res) + "/values/strings.xml")
-        Output.add_printer_callback("tree", "ApiKeyDetector", "Manifest", mprint)
-        Output.add_printer_callback("tree", "ApiKeyDetector", "source-code", mprint)
-        if string_file.is_file():
-            tree = ET.parse(string_file)
-            strings = tree.getroot().findall('string')
-            for s in strings:
-                try:
-                    if detector.detect_api_keys([s.text])[0]:
-                        Output.add_tree_mod("ApiKeyDetector", "Manifest",
-                                [s.get('name'), s.text])
 
-                        debug(f"{s.get('name')} : {s.text}")
-                        #print(logging.getLogger("phone_analysis"))
-                        #debug(s.get("name"))
-                except Exception as e:
-                    pass
+        Output.add_printer_callback("tree", "ApiKeyDetector", "source-code", mprint)
+
+        p = Path(tmp_dir + "/decompiled_app/").glob("**/*")
+        for string_file in p:            
+            failed = True
+            if not string_file.is_file():
+                continue
+
+            Output.add_printer_callback("tree", "ApiKeyDetector", str(string_file), mprint)
+
+            if '.xml' in string_file.suffixes:
+                try:
+                    tree = ET.parse(string_file)
+                    strings = tree.getroot().findall('string')
+                    for s in strings:
+                        try:
+                            if detector.detect_api_keys([s.text])[0]:
+                                Output.add_tree_mod("ApiKeyDetector", str(string_file), [s.get('name'), s.text])
+                                debug(f"{string_file.absolute()} : {s.get('name')} : {s.text}")
+                        except Exception as e:
+                            pass
+                except ET.ParseError as e:
+                    failed = False
+
+            
+            # Let's handle classic file from the whitelist extension
+            if failed and (not string_file.suffixes or string_file.suffixes[0] in whitelist_extension):
+                f = open(string_file, 'rb')
+                data = f.read()
+                f.close()
+
+                quoted = re.compile('"[^"]*"')
+                for value in quoted.findall(str(data)):
+                    try:
+                        value = value[1:-1]
+                        if detector.detect_api_keys([value])[0]:
+                            Output.add_tree_mod("ApiKeyDetector", str(string_file), ["", value])
+                            debug(f"{string_file.absolute()} : {value}")
+                    except Exception as e:
+                        pass
+
         from . import keys
 
 def mprint(arg : list) -> str:
